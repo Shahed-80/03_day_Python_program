f475b72c8edf5de1902c4745d22955d32b7f9778
Shahed-80 <107167352+Shahed-80@users.noreply.github.com>
2022-06-13 10:17:39 +0530

    Add files via upload

---
+++ Print_statement_day_1.py
@@ -0,0 +1,57 @@
+# ctrl + \ to remove or add a single line comment
+
+# For multi line comment we can indicate with ''' '''
+#                    or with """ """
+
+# Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+
+# Indentation refers to the spaces at the beginning of a code line.
+
+# Python uses indentation to indicate a block of code.
+
+# print("Hello world")
+# print('hii')
+# print()
+# print('Im iro\' man')
+
+
+''' Hel'lo world'''
+
+# print("Hel'lo world")
+# print("Hel\'lo world")
+# print("""Hel'lo world""")
+# print('''Hel'lo world''')
+# print('Hel\'lo world')
+      
+
+# print('Python \
+# \
+# programming')
+
+# print('I\'m a programmer')
+# print(r'I\'m a programmer')
+
+
+'''We can multiply the str with the int only'''
+
+# print("I'm iron man "   *  9)
+
+# print(True  * "Python")
+# print(False  * "Python")
+
+'''We cannot multiply the str with the str and float'''
+
+
+# print(5.6 * 'Number')
+
+# print('Python ' * 9.6)
+# print('''Pyth\'on ''' * 9.8)
+
+
+
+
+
+
+
+
